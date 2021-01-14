9c2f8a0f22da71fda7a3fa3c4bda1ae362608ce4
Charles Lai <charlesangeloplai@gmail.com>
2021-01-14 14:56:25 -0500

    commit tree fff4223172368300a7b455215e650c45614ad072

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,12 +4,29 @@ class PostsController < ApplicationController
   end
 
   def show
-    @post = Post.find(params[:id])
+    find_post
   end
-
+  
   def new
     @post = Post.new
   end
-
+  
   # add create method here
+  def create
+    @post = Post.new(post_params)
+    if @post.save
+      redirect_to posts_path
+    else
+      render :new
+    end
+  end
+  
+  private
+    def find_post
+      @post = Post.find(params[:id])
+    end
+
+    def post_params
+      params.require(:post).permit(:title, :description)
+    end
 end
